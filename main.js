bdffb50cc1b212b7e8bd742a41f61e12f7b55170
QodirjonAnorqulov <qodirjonanorqulov@gmail.com>
2022-09-22 23:54:01 +0500

    vazilarr

---
+++ main.js
@@ -0,0 +1,148 @@
+
+
+
+
+
+
+// // console.log(document.URL);
+// // console.log(document.domain);
+// // console.log(document.title);
+// // console.log(document.doctype);
+// // console.log(document.body);
+// // console.log(document.all);
+// // console.log(document.all[10]);
+// // document.all[10].textContent="hello group of developers"
+// // console.log(document.forms[0]);
+// // console.log(document.links);
+// // console.log(document.images);
+// // const title = document.getElementsByClassName("title");
+// // title[0].textContent = "Toxirxon";
+// // const subtitle = document.getElementById("header-title");
+// // subtitle.textContent ="toxaaaaaa";
+// // console.log(subtitle);
+
+
+// // const list = document.querySelectorAll("li")
+// // console.log(list);
+// // const li = document.getElementsByTagName("li")
+// // li[0].style.backgroundColor ="green"
+// // for( let i = 0 ; i <li.length ; i++){
+//     //     li[i].style.backgroundColor ="yellow"
+//     // }
+    
+//     // console.log(document.URL);
+//     // console.log(document.domain);
+//     // console.log(document.title);
+//     // console.log(document.doctype);
+//     // console.log(document.body);
+//     // console.log(document.all);
+//     // console.log(document.all[10]);
+//     // document.all[10].textContent="hello group of developers"
+//     // console.log(document.forms[0]);
+//     // console.log(document.links);
+//     // console.log(document.images);
+//     let persons = [
+//         {id:1,username:'axror',age:20,image:'https://picsum.photos/200/300'},
+//         {id:2,username:'toxirbek',age:18,image:'https://picsum.photos/200/300'},
+//         {id:3,username:'qodirjon',age:21,image:'https://picsum.photos/200/300'},
+//         {id:4,username:'alisher',age:20,image:'https://picsum.photos/200/300'},
+//         {id:5,username:'ahmadjon',age:20,image:'https://picsum.photos/200/300'},
+//         {id:6,username:'begzod',age:11,image:'https://picsum.photos/200/300'},
+//         {id:7,username:'mirjalol',age:14,image:'https://picsum.photos/200/300'},
+//         {id:8,username:'abduraxmon',age:20,image:'https://picsum.photos/200/300'},
+//         {id:9,username:'jamoliddinbek',age:18,image:'https://picsum.photos/200/300'},
+//         {id:10,username:"mo'minxoja",age:14,image:'https://picsum.photos/200/300'},
+//         {id:11,username:'abdurazzoq',age:18,image:'https://picsum.photos/200/300'}
+//     ]
+//     let containerEl = document.getElementById("container") 
+//        for(let  i =0; i<persons.length; i++){
+//         let cardelm = document.createElement("div")
+//         cardelm.style.width ="350px"
+       
+
+
+       
+//         let imgEL =document.createElement('img')
+//         imgEL.src =persons[i].image
+//         imgEL.style.borderRadius=("40%")
+        
+        
+        
+//         let h3EL =document.createElement('h3')
+//         h3EL.textContent= persons[i].username
+//         h3EL.style.color=("yellowgreen")
+//         h3EL.style.fontSize =('42px')
+
+//         let pEL =document.createElement('p')
+//         pEL.textContent =persons[i].age
+//         pEL.style.fontSize =('22px')
+//         pEL.style.color=("blue")
+
+
+
+
+//         cardelm.append(imgEL,h3EL,pEL)
+//         containerEl.append(cardelm)
+//     }
+// function ismtekshir(ism){
+//     if(typeof ism ==='string' && ism.length>0)
+//     {return true
+// } else{
+//     return false
+// }
+// }
+
+
+// function magazine(...params){
+   
+//     for(let i =0 ;i<params.length; i++){
+//       let newobj = {}
+//       newobj.id = params[0]
+//       newobj.namee = params[1]
+//       newobj.age = params[2]
+//     } 
+//    return params.l
+// }
+
+// let user =  magazine('olma', 'banan','olma', 'nok', 'banan')
+
+// function test50(x,y)
+// {
+//     return ((x == 50 || y == 50) ||( x,y ==="string"));
+//   }
+//   console.log(test50(50, 50))
+//   console.log(test50(20, 50))
+//   console.log(test50(20, 20))
+//   console.log(test50(20, 30))
+
+
+
+// function myFunction(a,n){
+    // let string = a;
+    // let index = n;
+    // return string.charAt(index);
+// }
+// myFunction(string,3)
+
+
+//masala 17
+
+
+// let n =6
+// // course =0
+// for(let i = 1; i<n; i++){
+// //    course +=n*i
+//     console.log(i*n);
+// }
+                //18
+                // function factorial(n){
+                //     if (n==0){
+                //         return 1
+                //     }
+                //     else{
+                //         return factorial(n-1)*n}
+                // }
+                // console.log(factorial(5));
+
+    
+    
